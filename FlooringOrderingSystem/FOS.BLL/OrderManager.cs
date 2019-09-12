b40a0c1225e8f4130004b7adf28701558abf320e
tjuranek <thomas@juranek.com>
2019-09-12 15:31:35 -0500

    app config, add in BLL, not tested yet

---
+++ FlooringOrderingSystem/FOS.BLL/OrderManager.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using FOS.Models;
+using FOS.Models.Responses;
+using FOS.Models.Interfaces;
+
+namespace FOS.BLL
+{
+    public class OrderManager
+    {
+        private IOrderRepository _orders;
+        private IProductRepository _products;
+        private ITaxRepository _taxes;
+
+        public OrderManager(IOrderRepository orders, IProductRepository products, ITaxRepository taxes)
+        {
+            _orders = orders;
+            _products = products;
+            _taxes = taxes;
+        }
+
+        #region CRUD methods
+        public Response CreateOrder(Order order)
+        {
+            Response response = new Response();
+
+            if (_orders.CreateOrder(order))
+                response.Success = true;
+            else
+            {
+                response.Success = false;
+                response.Message = "Error: Unable to create order.";
+            }
+
+            return response;
+        }
+
+        public Response RetrieveOrder(Order order)
+        {
+            Response response = new Response();
+            Order retrieved = _orders.RetrieveOrder(order);
+
+            if (retrieved != null)
+            {
+                response.Success = true;
+                response.Order = retrieved;
+            }
+            else
+            {
+                response.Success = false;
+                response.Message = "Error: Unable to retrieve order.";
+            }
+
+            return response;
+        }
+
+        public Response RetrieveOrdersByDate(DateTime date)
+        {
+            Response response = new Response();
+            List<Order> retrieved = _orders.RetrieveOrdersByDate(date);
+
+            if (retrieved != null || retrieved.Count != 0)
+            {
+                response.Success = true;
+                response.OrderList = retrieved;
+            }
+            else
+            {
+                response.Success = false;
+                response.Message = "Error: Unable to retrieve orders.";
+            }
+
+            return response;
+        }
+
+        public Response UpdateOrder(Order order)
+        {
+            Response response = new Response();
+
+            if (_orders.UpdateOrder(order))
+                response.Success = true;
+            else
+            {
+                response.Success = false;
+                response.Message = "Error: Unable to update order.";
+            }
+
+            return response;
+        }
+
+        public Response DeleteOrder(Order order)
+        {
+            Response response = new Response();
+
+            if (_orders.DeleteOrder(order))
+                response.Success = true;
+            else
+            {
+                response.Success = false;
+                response.Message = "Error: Unable to delete order.";
+            }
+
+            return response;
+        }
+        #endregion
+
+        public Order GenerateOrder(DateTime orderDate, string customerName, string state, string productType, decimal area)
+        {
+            Order order = new Order();
+
+            order.OrderNumber = 1; //default, data repo will take care of this
+            order.CustomerName = customerName;
+            order.State = state;
+            order.ProductType = productType;
+            order.Area = area;
+            order.OrderDate = orderDate;
+
+            foreach (Product product in _products.RetrieveProducts())
+            {
+                if (product.ProductType.ToLower() == order.ProductType.ToLower())
+                {
+                    order.CostPerSquareFoot = product.CostPerSquareFoot;
+                    order.LaborCostPerSquareFoot = product.LaborCostPerSquareFoot;
+                }
+            }
+
+            foreach (Tax tax in _taxes.RetrieveTaxes())
+            {
+                if (tax.StateAbbreviation.ToLower() == order.State.ToLower())
+                    order.TaxRate = tax.TaxRate;
+            }
+
+            order.MaterialCost = order.Area * order.CostPerSquareFoot;
+            order.LaborCost = order.Area * order.LaborCostPerSquareFoot;
+            order.Tax = (order.MaterialCost + order.LaborCost) * (order.TaxRate / 100);
+            order.Total = order.MaterialCost + order.LaborCost + order.Tax;
+
+            return order;
+        }
+    }
+}
---
+++ FlooringOrderingSystem/FOS.BLL/OrderManagerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FOS.Data.Development;
+using FOS.Data.Production;
+using System.Configuration;
+
+namespace FOS.BLL
+{
+    public class OrderManagerFactory
+    {
+        public static OrderManager Create()
+        {
+            string mode = ConfigurationManager.AppSettings["Mode"].ToString();
+
+            switch (mode)
+            {
+                case "Dev":
+                    return new OrderManager(new DevelopmentOrdersRepository(), new DevelopmentProductsRepository(), new DevelopmentTaxesRepository();
+                case "Prod":
+                    return new OrderManager(new ProductionOrdersRepository(@"C:\Data\LiveData"), new ProductionProductsRepository(@"C:\Data\LiveData"), new ProductionTaxesRepository(@"C:\Data\LiveData"));
+                default:
+                    throw new Exception("Mode value in app config is not valid");
+            }
+        }
+    }
+}
